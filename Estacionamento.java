b8e286ed692238ef5dc471ca0fdc8c0009ab8d79
CueiaDev <72366689+CueiaDev@users.noreply.github.com>
2021-03-12 15:32:18 -0300

    Update Estacionamento.java

--- Estacionamento.java
+++ Estacionamento.java
@@ -3,7 +3,7 @@ import java.awt.Color;
 public class Estacionamento{
   private Carro[] carros;
   
-  public Carro criarCarro(String placa){
-    return new Carro(Color.black, placa, 120);
+  public void novoCarro(Carro carro){
+    carros += carro;
   }
 }
